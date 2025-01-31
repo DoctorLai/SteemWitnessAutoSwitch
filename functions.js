d4f2302851cf605d39fef059c96ae7913604e2e0
DoctorLai <dr.zhihua.lai@gmail.com>
2025-01-31 20:12:34 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,20 +1,23 @@
-module.exports.sleep = function(time) {
-	return new Promise((resolve) => setTimeout(resolve, time));
+module.exports.sleep = function (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-module.exports.log = function (msg) { 
-  console.log(new Date().toISOString().replace("T", " ").substr(0, 19), msg); 
-}                          
+module.exports.log = function (messages) {
+  // Get the current timestamp
+  const timestamp = new Date().toISOString();
+  // Format the messages with the timestamp
+  console.log(`[${timestamp}]`, ...messages);
+}
 
-module.exports.runInterval = function(func, wait, times){
-  let interv = function(w, t){
-    return function(){
-      if (typeof t === "undefined" || t-- > 0){
+module.exports.runInterval = function (func, wait, times) {
+  let interv = function (w, t) {
+    return function () {
+      if (typeof t === "undefined" || t-- > 0) {
         setTimeout(interv, w);
         try {
           func.call(null);
         }
-        catch(e){
+        catch (e) {
           t = 0;
           throw e.toString();
         }
@@ -25,7 +28,7 @@ module.exports.runInterval = function(func, wait, times){
 };
 
 // check if y array includes any x 
-module.exports.arrayInArray = function(x, y) {
+module.exports.arrayInArray = function (x, y) {
   if (!x) { return false; }
   if (!y) { return false; }
   if (!x.constructor === Array) return false;
@@ -35,13 +38,13 @@ module.exports.arrayInArray = function(x, y) {
   return x.some(r => y.includes(r));
 }
 
-module.exports.shuffle = function(a) {
-    var j, x, i;
-    for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
-    }
-    return a;
+module.exports.shuffle = function (a) {
+  let j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
 }
